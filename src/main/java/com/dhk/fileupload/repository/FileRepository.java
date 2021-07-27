11f10c6287c93c76e05f3fb1d5646e33b95de672
kimdaehyun <zbeld123@gmail.com>
2021-07-27 14:23:22 +0900

    값타입 컬렉션을 적용하여 파일을 포함한 Item DB에 저장까지 확인

--- src/main/java/com/dhk/fileupload/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dhk.fileupload.repository;
-
-import com.dhk.fileupload.domain.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Long> {
-}
---
+++ src/main/java/com/dhk/fileupload/response/ItemResponse.java
@@ -0,0 +1,2 @@
+package com.dhk.fileupload.response;public class ItemResponse {
+}
